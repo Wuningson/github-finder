02ba5d3579790e62699a0aacaf03db8e6b23a818
Wuningson <christdam55@gmail.com>
2020-03-16 12:49:16 +0100

    All done with refactoring to hooks and context API

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Search = ({ clearUsers, showClear, searchUsers, showAlert }) => {
+const Search = () => {
+  const { searchUsers, users, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
   const [text, setText] = useState('');
   // Since there we can have multiple forms and won't want to write on change even for each we can do e.target.name
   // to access the name of the form and then set the state for the form
   const onSubmit = e => {
     e.preventDefault();
     if (text === '') {
-      showAlert('Please enter Something', 'light');
+      setAlert('Please enter Something', 'light');
     } else {
       searchUsers(text);
       setText('');
@@ -29,20 +32,13 @@ const Search = ({ clearUsers, showClear, searchUsers, showAlert }) => {
         />
         <input type='submit' name='Search' className='btn btn-dark btn-block' />
       </form>
-      {showClear && (
+      {users.length > 0 && (
         <button className='btn btn-light btn-block' onClick={clearUsers}>
           Clear
         </button>
       )}
     </div>
   );
-};
-
-Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  showAlert: PropTypes.func.isRequired
 };
 
 export default Search;
